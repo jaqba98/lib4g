f75caa79baa23a49c2c2a6fa37c493db64c59054
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2022-03-14 17:42:29 +0100

    Create basic window class

--- lib4g/src/lib4g.cpp
+++ lib4g/src/lib4g.cpp
@@ -1,5 +1,6 @@
 #include "lib4g.hpp"
 
+#include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
@@ -8,7 +9,7 @@ namespace lib4g {
 		this->width = width;
 		this->height = height;
 		this->title = title;
-		
+
 		glfwInit();
 		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -17,7 +18,25 @@ namespace lib4g {
 		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
 #endif
 		GLFWwindow* window = glfwCreateWindow(this->width, this->height, this->title, NULL, NULL);
+		if (window == NULL) {
+			std::cout << "Failed to create window" << std::endl;
+			glfwTerminate();
+			return;
+		}
+		glfwMakeContextCurrent(window);
+		if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+			std::cout << "Failed to initalize glad" << std::endl;
+			return;
+		}
+		// glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+		glViewport(0, 0, this->width, this->height);
 		while (!glfwWindowShouldClose(window)) {
+			if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+				glfwSetWindowShouldClose(window, true);
+			}
+			glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+			glClear(GL_COLOR_BUFFER_BIT);
+			glfwSwapBuffers(window);
 			glfwPollEvents();
 		}
 		glfwTerminate();
