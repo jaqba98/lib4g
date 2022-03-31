d2f447f4aad7bb70683ca7646001cda47d592c40
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2022-03-31 17:33:30 +0200

    Create game init

--- lib4g/src/core.cpp
+++ lib4g/src/core.cpp
@@ -1,3 +1,5 @@
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
 #include <iostream>
 #include "input-validation.hpp"
 #include "core.hpp"
@@ -6,6 +8,17 @@ namespace lib4g {
 	void Core::run(int width, int height, std::string title) {
 		lib4g::validations::InputValidation* inputValidation = new lib4g::validations::InputValidation();
 		inputValidation->validation(width, height, title);
+		init();
 		delete inputValidation;
+	}
+
+	void Core::init() {
+		glfwInit();
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
+#endif
 	}
 }
--- lib4g/src/core.hpp
+++ lib4g/src/core.hpp
@@ -4,5 +4,6 @@ namespace lib4g {
 	class Core {
 	public:
 		void run(int width, int height, std::string title);
+		void init();
 	};
 }
