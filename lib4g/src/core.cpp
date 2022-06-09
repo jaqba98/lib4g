e467bd5a6e0b759b960546188181d4aa2c4206bf
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2022-06-09 19:36:39 +0200

    create basic rectangle

--- lib4g/src/core.cpp
+++ lib4g/src/core.cpp
@@ -57,24 +57,28 @@ namespace lib4g {
 			glDeleteShader(vertexShader);
 			glDeleteShader(fragmentShader);
 
-			GLfloat vertices[] = {
-				-0.5f, -0.5f, 0.0f,
+			float vertices[] = {
+				 0.5f,  0.5f, 0.0f,
 				 0.5f, -0.5f, 0.0f,
-				 0.0f,  0.5f, 0.0f
+				-0.5f,  0.5f, 0.0f,
+				 0.5f, -0.5f, 0.0f,
+				-0.5f, -0.5f, 0.0f,
+				-0.5f,  0.5f, 0.0f
 			};
 
 			GLuint VAO, VBO;
+
 			glGenVertexArrays(1, &VAO);
 			glGenBuffers(1, &VBO);
-
+			
 			glBindVertexArray(VAO);
-
+			
 			glBindBuffer(GL_ARRAY_BUFFER, VBO);
 			glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
 			glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 			glEnableVertexAttribArray(0);
-
+			
 			glBindBuffer(GL_ARRAY_BUFFER, 0);
 			glBindVertexArray(0);
 
@@ -82,11 +86,9 @@ namespace lib4g {
 				processInput(windowBuilder->window);
 				glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 				glClear(GL_COLOR_BUFFER_BIT);
-
 				glUseProgram(shaderProgram);
 				glBindVertexArray(VAO);
-				glDrawArrays(GL_TRIANGLES, 0, 3);
-
+				glDrawArrays(GL_TRIANGLES, 0, 6);
 				glfwSwapBuffers(windowBuilder->window);
 				glfwPollEvents();
 			}
